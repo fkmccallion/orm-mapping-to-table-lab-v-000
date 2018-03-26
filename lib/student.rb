f37ebd62475980d5fa07a5070ae85424d651115e
Frank McCallion <fkmccallion@gmail.com>
2018-03-26 02:46:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,5 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
-
   attr_accessor :name, :grade
   attr_reader :id
 
@@ -33,9 +30,9 @@ class Student
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
-    SQL    
+    SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    
+
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
